c57e27e75f925448ab98271b2a47f09ebc2e7149
Tissiana Alves <altissiana@gmail.com>
2019-11-04 01:10:14 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,7 +2,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
-  pp nds
+  pp directors_database
   {}
   directors_database.each do |director|
     director[]
