7a7709e4e83320eee67800ee145f4aa1fec185ef
Tissiana Alves <altissiana@gmail.com>
2019-11-04 01:41:31 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,11 +4,12 @@ require 'directors_database'
 def directors_totals(nds)
   list = {}
   nds.each do |director|
+    name = director[:name]
     total = 0 
     director[:movies].each do |movie|
       total = total + movie[:worldwide_gross]
     end
-      list[director[:name]] = total
+      list[name] = total
   end
   list
 end
