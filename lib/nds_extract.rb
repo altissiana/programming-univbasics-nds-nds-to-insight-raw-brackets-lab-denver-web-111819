db3d64f619349e9a7e1b7f1864f0e86653fb2eed
Tissiana Alves <altissiana@gmail.com>
2019-11-04 01:35:58 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,15 +1,14 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'directors_database'
+#require 'directors_database'
 
 def directors_totals(nds)
-  pp directors_database
-  {}
-  directors_database.each do |director|
+  list = {}
+  nds.each do |director|
     total = 0 
     director[:movies].each do |movie|
       total = total + movie[:worldwide_gross]
     end
-    total
-    
+      list[director] = total
   end
+      list
 end
